4488f9b5e2dda6aebe4abd7e72efd78478782fc7
Renwarren <warren.chendjou@gmail.com>
2024-12-11 02:22:18 -0500

    Add static typing

--- database.py
+++ database.py
@@ -9,8 +9,7 @@ def get_db():
     finally:
         db.close()
 
-DATABASE_URL = "sqlite:///./users.db"
+DATABASE_URL:str = "sqlite:///./local.db"
 # Create the database engine
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
