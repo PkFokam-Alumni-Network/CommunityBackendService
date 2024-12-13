d9c93c7e5b4a7e97dce0f503eaf98b23aed9a1b1
Renwarren <warren.chendjou@gmail.com>
2024-12-12 23:38:23 -0500

    Refactor to use optional

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,22 +1,26 @@
-from pydantic import BaseModel, EmailStr
+from typing import Optional
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 class UserCreate(BaseModel):
     email: EmailStr
     first_name: str
     last_name: str
-    graduation_year: int
-    degree: str
-    major: str
-    phone: str
     password: str
-    current_occupation: str
-    image: str
-    linkedin_profile: str
+    graduation_year: Optional[int] = None
+    degree: Optional[str] = None
+    major: Optional[str] = None
+    phone: Optional[str] = None
+    current_occupation: Optional[str] = None
+    image: Optional[str] = None
+    linkedin_profile: Optional[str] = None
+    mentor_email: Optional[str] = None
 
-class UserOut(BaseModel):
+class UserCreatedResponse(BaseModel):
     email: str
     first_name: str
     last_name: str
+    
+    model_config = ConfigDict(from_attributes=True)
 
-class DeletionApproved(BaseModel):
+class UserDeletedResponse(BaseModel):
     message: str
