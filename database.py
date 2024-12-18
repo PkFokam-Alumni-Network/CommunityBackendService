3d8119dbe6a96d84b871419dbe32726ccc7eaa5b
evil-star25 <kiracedric05@gmail.com>
2024-12-18 09:28:22 -0500

    Update to use environment variable for test database URL

--- database.py
+++ database.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path=".database_url")
-DATABASE_URL = os.getenv("DATABASE_URL")
+load_dotenv(dotenv_path=".database_url.example")
+DATABASE_URL = os.getenv("DATABASE_URL",f"sqlite:///{tempfile.NamedTemporaryFile(suffix='.db', delete=False).name}")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set!")
 
