727dee33d063dcf4e0b62ce625bd4b080cbdcd39
Renwarren <warren.chendjou@gmail.com>
2025-02-19 01:12:18 -0500

    Add an auto-increment id

--- models/user.py
+++ models/user.py
@@ -9,26 +9,23 @@ class UserRole(enum.Enum):
 
 class User(Base):
     __tablename__ = "users"
-    email = Column(String, primary_key=True, unique=True, index=True)
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String, unique=True, index=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
     password = Column(String, nullable=False)
-    
     address = Column(String)
     phone = Column(String)
     image = Column(String, nullable=True)
     bio = Column(String)
-
     graduation_year = Column(Integer)
     degree = Column(String)
     major = Column(String)
-
     current_occupation = Column(String)
     mentor_email = Column(String, ForeignKey("users.email"), nullable=True)
     mentor = relationship("User", remote_side=[email], backref="mentees", foreign_keys=[mentor_email])
-    
     is_active = Column(Boolean, default=True)
-
     linkedin_profile = Column(String)
     instagram_profile = Column(String, nullable=True)
     role = Column(Enum(UserRole), default=UserRole.user, nullable=False)
