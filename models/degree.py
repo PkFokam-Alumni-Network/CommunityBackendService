62e45855aaeef703f9c82aaf46b875f740f84123
Dharld <yanndjoumessi@gmail.com>
2025-05-19 15:39:15 -0700

    Created backup

---
+++ models/degree.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Degree(Base):
+    __tablename__ = "degrees"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False) # Foreign key to users table
+    degree = Column(String, nullable=False)
+    major = Column(String, nullable=False)
+    graduation_year = Column(Integer, nullable=False)
+    location = Column(String, nullable=False)
+
+    user = relationship("User", back_populates="degrees")
