d38a37fcb6a391100db935f75d4a890637b5e87b
evil-star25 <kiracedric05@gmail.com>
2025-05-20 17:07:16 -0400

    updated which refactor all the use of env variables to use settings class

--- auth.py
+++ auth.py
@@ -1,17 +1,18 @@
-import os, secrets
+import secrets
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi import Depends, HTTPException, status
 from typing import Annotated
+from settings import settings
 
 security = HTTPBasic()
 def get_current_username(credentials: Annotated[HTTPBasicCredentials, Depends(security)]) -> str:
     current_username_bytes = credentials.username.encode("utf8")
-    correct_username_bytes = os.getenv("DOCS_AUTH_USERNAME").encode("utf8")
+    correct_username_bytes = settings.DOCS_AUTH_USERNAME.encode("utf8")
     is_correct_username = secrets.compare_digest(
         current_username_bytes, correct_username_bytes
     )
     current_password_bytes = credentials.password.encode("utf8")
-    correct_password_bytes = os.getenv("DOCS_AUTH_PASSWORD").encode("utf8")
+    correct_password_bytes = settings.DOCS_AUTH_PASSWORD.encode("utf8")
     is_correct_password = secrets.compare_digest(
         current_password_bytes, correct_password_bytes
     )
