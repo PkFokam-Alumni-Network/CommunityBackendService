aff3f872a6d6ec21cc4f9ed3a18fd30eb8ce945c
Warren Fongang <70856761+Renwarren@users.noreply.github.com>
2025-05-20 11:33:30 -0400

    Update start.sh
    Signed-off-by: Warren Fongang <70856761+Renwarren@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 
 
 # Run database migrations
-alembic upgrade head
+# alembic upgrade head
 
 # Start the FastAPI app
 exec uvicorn main:app --host 0.0.0.0 --port 9000 --proxy-headers --forwarded-allow-ips "*"
