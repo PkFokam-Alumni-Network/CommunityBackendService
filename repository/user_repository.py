ed1bc51e86e333f0213981d0894933f80e493c1d
Warren Fongang <70856761+Renwarren@users.noreply.github.com>
2025-05-23 13:22:22 -0400

    Add retry mechanism for polling (#140)

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -3,11 +3,13 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError, OperationalError
 from models.user import User
 from utils.singleton_meta import SingletonMeta
+from utils.retry import retry_on_db_error
 
 class UserRepository(metaclass=SingletonMeta):
     def __init__(self, session: Session):
         self.db: Session = session
 
+    @retry_on_db_error()
     def add_user(self, user: User) -> User:
         try:
             self.db.add(user)
@@ -24,17 +26,21 @@ class UserRepository(metaclass=SingletonMeta):
             self.db.rollback()
             raise RuntimeError(f"An error occurred: {e}")
     
+    @retry_on_db_error()
     def get_user_by_id(self, user_id: int) -> Optional[User]:
         return self.db.query(User).filter(User.id == user_id).first()
 
+    @retry_on_db_error()
     def get_user_by_email(self, email: str) -> Optional[User]:
         return self.db.query(User).filter(User.email == email).first()
 
+    @retry_on_db_error()
     def get_users(self, active) -> List[User]:
         if active:
             return self.db.query(User).filter(User.is_active == True).all()
         return self.db.query(User).all()
 
+    @retry_on_db_error()
     def update_user(self, user: User) -> Optional[User]:
         try:
             self.db.merge(user)
@@ -48,6 +54,7 @@ class UserRepository(metaclass=SingletonMeta):
             self.db.rollback()
             raise RuntimeError(f"An error occurred: {e}")
 
+    @retry_on_db_error()
     def delete_user(self, user_id: int) -> None:
         user = self.get_user_by_id(user_id)
         if not user:
@@ -63,6 +70,7 @@ class UserRepository(metaclass=SingletonMeta):
             self.db.rollback()
             raise RuntimeError(f"An error occurred while deleting the user: {e}")
 
+    @retry_on_db_error()
     def get_all_mentees(self, mentor_id: int) -> List[User]:
         user = self.get_user_by_id(mentor_id)
         if not user:
