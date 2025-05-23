4f6053e38421426c31b439f83a4c5f41eaf8c7d7
Renwarren <warren.chendjou@gmail.com>
2025-05-23 13:19:33 -0400

    Add retry mechanism for polling

--- repository/announcement_repository.py
+++ repository/announcement_repository.py
@@ -3,12 +3,14 @@ from sqlalchemy.orm import Session
 from models.announcement import Announcement
 from schemas.announcement_schema import AnnouncementCreate, AnnouncementUpdate
 from utils.singleton_meta import SingletonMeta
+from utils.retry import retry_on_db_error
 
 
 class AnnouncementRepository(metaclass=SingletonMeta):
     def __init__(self, session: Session):
         self.session = session
 
+    @retry_on_db_error()
     def add_announcement(self, announcement: AnnouncementCreate) -> Announcement:
         db_announcement = Announcement(**announcement.model_dump())
         self.session.add(db_announcement)
@@ -16,12 +18,15 @@ class AnnouncementRepository(metaclass=SingletonMeta):
         self.session.refresh(db_announcement)
         return db_announcement
 
+    @retry_on_db_error()
     def get_announcements(self) -> list[Type[Announcement]]:
         return self.session.query(Announcement).all()
 
+    @retry_on_db_error()
     def get_announcement_by_id(self, announcement_id: int) -> Optional[Announcement]:
         return self.session.query(Announcement).filter(Announcement.id == announcement_id).first()
 
+    @retry_on_db_error()
     def update_announcement(self, announcement_id: int, announcement: AnnouncementUpdate) -> Optional[Announcement]:
         db_announcement = self.get_announcement_by_id(announcement_id)
         if db_announcement:
@@ -31,6 +36,7 @@ class AnnouncementRepository(metaclass=SingletonMeta):
             self.session.refresh(db_announcement)
         return db_announcement
 
+    @retry_on_db_error()
     def delete_announcement(self, announcement_id: int) -> Optional[Announcement]:
         db_announcement = self.get_announcement_by_id(announcement_id)
         if db_announcement:
@@ -38,5 +44,6 @@ class AnnouncementRepository(metaclass=SingletonMeta):
             self.session.commit()
         return db_announcement
 
+    @retry_on_db_error()
     def get_announcement_by_title(self, title: str) -> Optional[Announcement]:
         return self.session.query(Announcement).filter(Announcement.title == title).first()
