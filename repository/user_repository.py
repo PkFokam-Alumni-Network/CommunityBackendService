16f0fb5d3162a56c74c42d7ab12aed74cd964aee
Renwarren <warren.chendjou@gmail.com>
2025-05-23 16:28:14 -0400

    Fix non thread safe session

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -6,77 +6,75 @@ from utils.singleton_meta import SingletonMeta
 from utils.retry import retry_on_db_error
 
 class UserRepository(metaclass=SingletonMeta):
-    def __init__(self, session: Session):
-        self.db: Session = session
 
     @retry_on_db_error()
-    def add_user(self, user: User) -> User:
+    def add_user(self, db: Session, user: User) -> User:
         try:
-            self.db.add(user)
-            self.db.commit()
-            self.db.refresh(user)
+            db.add(user)
+            db.commit()
+            db.refresh(user)
             return user
         except IntegrityError:
-            self.db.rollback()
+            db.rollback()
             raise ValueError(f"User with email {user.email} already exists.")
         except OperationalError as e:
-            self.db.rollback()
+            db.rollback()
             raise ConnectionError("Database connection error: " + str(e))
         except Exception as e:
-            self.db.rollback()
+            db.rollback()
             raise RuntimeError(f"An error occurred: {e}")
     
     @retry_on_db_error()
-    def get_user_by_id(self, user_id: int) -> Optional[User]:
-        return self.db.query(User).filter(User.id == user_id).first()
+    def get_user_by_id(self, db: Session, user_id: int) -> Optional[User]:
+        return db.query(User).filter(User.id == user_id).first()
 
     @retry_on_db_error()
-    def get_user_by_email(self, email: str) -> Optional[User]:
-        return self.db.query(User).filter(User.email == email).first()
+    def get_user_by_email(self, db: Session, email: str) -> Optional[User]:
+        return db.query(User).filter(User.email == email).first()
 
     @retry_on_db_error()
-    def get_users(self, active) -> List[User]:
+    def get_users(self, db: Session, active) -> List[User]:
         if active:
-            return self.db.query(User).filter(User.is_active == True).all()
-        return self.db.query(User).all()
+            return db.query(User).filter(User.is_active == True).all()
+        return db.query(User).all()
 
     @retry_on_db_error()
-    def update_user(self, user: User) -> Optional[User]:
+    def update_user(self, db: Session, user: User) -> Optional[User]:
         try:
-            self.db.merge(user)
-            self.db.commit()
-            self.db.refresh(user)
+            db.merge(user)
+            db.commit()
+            db.refresh(user)
             return user
         except IntegrityError:
-            self.db.rollback()
+            db.rollback()
             raise ValueError(f"User with email {user.email} already exists.")
         except Exception as e:
-            self.db.rollback()
+            db.rollback()
             raise RuntimeError(f"An error occurred: {e}")
 
     @retry_on_db_error()
-    def delete_user(self, user_id: int) -> None:
-        user = self.get_user_by_id(user_id)
+    def delete_user(self, db: Session, user_id: int) -> None:
+        user = self.get_user_by_id(db, user_id)
         if not user:
             raise ValueError("User not found.")
         
         try:
-            self.db.delete(user)
-            self.db.commit()
+            db.delete(user)
+            db.commit()
         except IntegrityError:
-            self.db.rollback()
+            db.rollback()
             raise ValueError("Unable to delete user due to integrity constraints.")
         except Exception as e:
-            self.db.rollback()
+            db.rollback()
             raise RuntimeError(f"An error occurred while deleting the user: {e}")
 
     @retry_on_db_error()
-    def get_all_mentees(self, mentor_id: int) -> List[User]:
-        user = self.get_user_by_id(mentor_id)
+    def get_all_mentees(self, db: Session, mentor_id: int) -> List[User]:
+        user = self.get_user_by_id(db, mentor_id)
         if not user:
             raise ValueError("User not found.")
         try:
-            mentees = self.db.query(User).filter(User.mentor_id == user.id).all()
+            mentees = db.query(User).filter(User.mentor_id == user.id).all()
             return mentees if mentees else []
         except Exception as e:
             raise RuntimeError(f"An error occurred while retrieving mentees: {e}")
