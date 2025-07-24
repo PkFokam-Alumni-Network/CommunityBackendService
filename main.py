90a5138db13252c2c57741558be08116d6951177
Warren Fongang <70856761+Renwarren@users.noreply.github.com>
2025-07-24 00:37:18 -0400

    Update main.py
    Signed-off-by: Warren Fongang <70856761+Renwarren@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from core.auth import get_current_username
 import core.database as database
 from sqlalchemy import text
 from core.database import get_db
-from routers import user_router, announcement_router, event_router
+from routers import user_router, announcement_router, event_router, post_router
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
