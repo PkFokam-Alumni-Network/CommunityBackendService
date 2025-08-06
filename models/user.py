026743eb5555bc5ad7cbd344d03281c58c88de2b
Warren Fongang <70856761+Renwarren@users.noreply.github.com>
2025-08-06 14:47:35 -0400

    Prevent auto table creation (#187)
    * Add alembic migration
    
    * Fix small issues

--- models/user.py
+++ models/user.py
@@ -31,7 +31,7 @@ class User(Base):
     instagram_profile = Column(Text, nullable=True)
     role = Column(Enum(UserRole), default=UserRole.user, nullable=False)
 
-    posts = relationship("Post", back_populates="author")
+    posts = relationship("Post", back_populates="author", cascade="all, delete-orphan")
     # comments = relationship("Comment", back_populates="author", cascade="all, delete-orphan")
     mentor = relationship("User", remote_side=[id], backref="mentees", foreign_keys=[mentor_id])
     user_events = relationship("UserEvent", back_populates="user", cascade="all, delete-orphan")
